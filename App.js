c5c3690dcbc9fcd4e90621960e667943fadbee21
Hetali21 <hetalibhimjiyani2014@gmail.com>
2022-03-24 10:15:40 +0530

    all code

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import MainStackNavigator from './src/navigation/AppNavigator'
+import { Provider } from 'react-redux'
+import store from './src/redux/store'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+    <MainStackNavigator />
+  </Provider>
+
   );
+  
+
+ 
 }
 
 const styles = StyleSheet.create({
